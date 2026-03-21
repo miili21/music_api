bce95433b2a994e5052248e5c4308ef60b4ecbb6
Mile <milenamartinez21a@gmail.com>
2026-03-21 15:21:35 -0400

    fix(api): Add unique index to artist_releases
    - Prevent duplicate artist-release pairs at database level
    - Fix race condition not handled by Rails uniqueness validation
    - Ensure uniqueness of artist_id scoped to release_id

---
+++ db/migrate/20260321180226_add_unique_index_to_artist_releases.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexToArtistReleases < ActiveRecord::Migration[8.0]
+  def change
+    add_index :artist_releases, [:artist_id, :release_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_182513) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_21_180226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_182513) do
     t.datetime "created_at", null: false
     t.bigint "release_id", null: false
     t.datetime "updated_at", null: false
+    t.index ["artist_id", "release_id"], name: "index_artist_releases_on_artist_id_and_release_id", unique: true
     t.index ["artist_id"], name: "index_artist_releases_on_artist_id"
     t.index ["release_id"], name: "index_artist_releases_on_release_id"
   end
